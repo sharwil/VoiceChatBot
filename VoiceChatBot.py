d9f0b97eeb0a0e7a5b8460bad67ba8c19c9539d3
sharwil <52228860+sharwil@users.noreply.github.com>
2021-04-23 15:39:07 +0530

    Add files via upload

---
+++ VoiceChatBot.py
@@ -0,0 +1,46 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+import speech_recognition as sr
+import pyttsx3
+
+bot= ChatBot('Bot')
+bot.set_trainer(ListTrainer)
+
+r= sr.Recognizer()
+source= sr.Microphone()
+
+try:
+    engine= pyttsx3.init()
+except ImportError:
+    print('Requested driver is not found')
+except RuntimeError:
+    print('Driver fails to initilize')
+
+voices= engine.getProperty('voices')
+for voice in voices:
+    print(voice.id)
+
+engine.setProperty('voice',
+                   'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
+
+for files in os.listdir(r'C:\Users\sharwil\Downloads\Prerequisite\chatterbot-corpus-master\chatterbot_corpus\data\english'):
+    data= open('C:/Users/sharwil/Downloads/Prerequisite/chatterbot-corpus-master/chatterbot_corpus/data/english/'+ files, 'r').readlines()
+    #bot.train(data)
+
+while True:
+    with sr.Microphone() as source:
+        print('say something')
+        audio = r.listen(source)
+        print(audio)
+        message= r.recognize_google(audio)
+        print(message)
+    if message.strip() != 'Bye':
+        reply = bot.get_response(message)
+
+    engine.say(reply)
+    engine.runAndWait()
+    if message.strip() == 'Bye':
+        engine.say('Bye')
+        engine.runAndWait()
+    break
